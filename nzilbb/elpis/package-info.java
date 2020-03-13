013f4c0188819fb612e73e2f220d1869f2a66c76
Robert Fromont <robert@fromont.net.nz>
2020-03-13 16:43:23 -0300

    Add CoEDL mentions/links to overview and package info.

--- nzilbb/elpis/package-info.java
+++ nzilbb/elpis/package-info.java
@@ -8,16 +8,20 @@
  *
  * <p>Elpis is a tool to obtain a first-pass automatic transcription on untranscribed
  *   audio, for manual correction.</p>
- *
+ * <p>Elpis was developed by the
+ *  <a href="https://www.dynamicsoflanguage.edu.au/">
+ *   Centre of Excellence for the Dynamics of Language (CoEDL)</a>
+ *  as part of the <em>Transcription Acceleration Project</em> (TAP) cross-disciplinary
+ *  project aimed at identifying the workflows of linguists and language workers during
+ *  transcription, and developing assistive tools to accelerate that process. </p> 
  * <p>It has a user-friendly browser-based interface, which can be used by researchers
  *   to upload training data, initiate model training, and provide recordings for
  *   automatic transcription.</p>
- *
  * <p>This GUI uses an API which is documented
- * <a href="https://github.com/nzilbb/elpis-java/blob/master/docs/README.md">here</a>.</p>
+ *  <a href="https://github.com/nzilbb/elpis-java/blob/master/docs/README.md">here</a>.</p>
  *     
- * <h2>What is this library?</h2>
+ * <h2>What is this package?</h2>
  *    
- * <p>The library provides programmatic access to Elpis API for Java.</p>
+ * <p>This package provides programmatic access to Elpis API for Java.</p>
  */
 package nzilbb.elpis;
